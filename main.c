ffdd7939c598d737fa203dbe20ef441957d5e72b
Mateo Mamut <vladom156@gmail.com>
2020-01-02 15:25:59 +0100

    Initial Commit

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+char* readFromConsole() {
+  char* buffer readline("\n>>> ");
+
+  return buffer;
+}
+
+
+
+
+
+
+
+int main(void) {
+  //Varijable za obradu podataka
+  char* input;
+  char** args;
+  int status;
+
+  //Glavna petlja koja ce se vrtit
+  do {
+    //citanje?
+    //parsiranje?
+    //Izvedba?
+  } while (status);
+
+  return 0;
+}
