d8b7948e134d514234db71596a04c17a5b193260
Mateo Mamut <vladom156@gmail.com>
2020-01-02 17:44:43 +0100

    Added some of the execute and part of the built ins, most logic for them is done but need to add more features

---
+++ built_ins.h
@@ -0,0 +1,50 @@
+#ifndef BUILT_INS_H
+#define BUILT_INS_H
+
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int change_dir(char **args);
+int help(char **args);
+int exit_shell(char **args);
+
+
+char *built_in_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*built_in_func[]) (char **) = {
+  &change_dir,
+  &help,
+  &exit_shell
+};
+
+int num_built_ins() {
+  return sizeof(built_in_str) / sizeof(char *);
+}
+
+int exit_shell(charr** args) {
+  return 0;
+}
+
+int change_dir(char* args) {
+  int status
+
+  return status;
+}
+
+int exec_built_in(char** arguments) {
+  int i, size = num_built_ins();
+
+  for (i = 0; i < size; i++) {
+    if (!strcmp(arguments[0], built_in_str[i]))
+      return (*built_in_func[i])(arguments);
+  }
+
+  return -1
+}
+
+#endif
--- main.c
+++ main.c
@@ -3,13 +3,48 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+#include "built_ins.h"
+
 char* readFromConsole() {
   char* buffer readline("\n>>> ");
 
   return buffer;
 }
 
+char** parseArguments(char* line) {
+  //Razdvajanje
 
+}
+
+int execute(char** args) {
+  //Izvrsavanje naredbi - nasi programi van patha?
+  //provjera aliasa
+
+  //Kao prvo gleda za built ins pa onda ostatak
+  if ((status = exec_built_in(args)) == -1) {
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+      // dijete
+      if (execvp(args[0], args) == -1) {
+        perror("lsh");
+      }
+      exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+      // neuspjel fork
+      perror("lsh");
+    } else {
+      // roditelj
+      do {
+        wpid = waitpid(pid, &status, WUNTRACED);
+      } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return status;
+  }
+}
 
 
 
@@ -23,9 +58,12 @@ int main(void) {
 
   //Glavna petlja koja ce se vrtit
   do {
-    //citanje?
-    //parsiranje?
+    input = readFromConsole();//zasad cemo rec da daje bas komandu a ne kobasicu teksta
+    //parsiranje? - Razdvajanje + svasta nesto kasnije
     //Izvedba?
+
+    //oslobadanje (treba vidit kako tocno readline radi)
+    free(input);
   } while (status);
 
   return 0;
